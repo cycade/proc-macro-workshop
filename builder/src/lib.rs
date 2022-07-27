dd69498c01014ae94df63b5538332d02da2f7012
Inaho <Inahocyc@gmail.com>
2022-07-27 23:12:13 +0800

    builder: 02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,17 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream};
+use syn;
+use quote::{self, ToTokens};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ts = syn::parse_macro_input!(input as syn::DeriveInput);
+    let ident_name = ts.ident.to_token_stream();
 
-    unimplemented!()
+    quote::quote! {
+        impl #ident_name {
+            fn builder() {
+                println!("create builder fn successfully!")
+            }
+        }
+    }.into()
 }
