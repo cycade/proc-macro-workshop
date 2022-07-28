c1c059d6c223ffdd2c03f0ce9c99ad35f64072c4
Inaho <Inahocyc@gmail.com>
2022-07-28 23:23:36 +0800

    builder: 03-call-setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,14 +4,40 @@ use quote::{self, ToTokens};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let ts = syn::parse_macro_input!(input as syn::DeriveInput);
-    let ident_name = ts.ident.to_token_stream();
+    let mut qoute_series: Vec<TokenStream> = vec![];
 
-    quote::quote! {
-        impl #ident_name {
-            fn builder() {
-                println!("create builder fn successfully!")
+    let input_stream = syn::parse_macro_input!(input as syn::DeriveInput);
+    let struct_ident = input_stream.ident.to_token_stream();
+    qoute_series.push(quote::quote! {
+        struct B {}
+        impl #struct_ident {
+            fn builder() -> B {
+                B{}
             }
         }
-    }.into()
+    }.into());
+
+    match input_stream.data {
+        syn::Data::Struct(data) => {
+            match data.fields {
+                syn::Fields::Named(fields) => {
+                    for f in fields.named.into_iter() {
+                        let fn_ident = f.ident.unwrap().to_token_stream();
+                        let param_ty = f.ty.to_token_stream();
+                        qoute_series.push(quote::quote! {
+                            impl B {
+                                fn #fn_ident(&mut self, input: #param_ty) -> () {}
+                            }
+                        }.into());
+                    }
+                },
+                _ => (),
+            }
+        },
+        _ => (),
+    }
+
+    let mut token_stream = proc_macro::TokenStream::new();
+    token_stream.extend(qoute_series.into_iter());
+    token_stream
 }
