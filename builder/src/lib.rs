d060171a70f98419cafeac86056ff7195a450038
Inaho <Inahocyc@gmail.com>
2022-08-06 11:19:24 +0800

    builder: 04-call-build

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,21 +1,23 @@
 use proc_macro::{TokenStream};
+use proc_macro2::{TokenStream as TStream, Ident};
 use syn;
-use quote::{self, ToTokens};
+use quote::{self, ToTokens, spanned::Spanned};
+
+fn create_stream(stream_series: Vec<TStream>) -> TStream {
+    let mut stream = TStream::new();
+    stream.extend(stream_series.into_iter());
+    stream
+}
+
+fn anomy_ident(ident: &TStream) -> Ident {
+    let tpl = format!("_{}", ident);
+    return syn::Ident::new(&tpl, ident.__span());
+}
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let mut qoute_series: Vec<TokenStream> = vec![];
-
+    let mut stream_pair: Vec<(TStream, TStream, Ident)> = vec![];
     let input_stream = syn::parse_macro_input!(input as syn::DeriveInput);
-    let struct_ident = input_stream.ident.to_token_stream();
-    qoute_series.push(quote::quote! {
-        struct B {}
-        impl #struct_ident {
-            fn builder() -> B {
-                B{}
-            }
-        }
-    }.into());
 
     match input_stream.data {
         syn::Data::Struct(data) => {
@@ -24,11 +26,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     for f in fields.named.into_iter() {
                         let fn_ident = f.ident.unwrap().to_token_stream();
                         let param_ty = f.ty.to_token_stream();
-                        qoute_series.push(quote::quote! {
-                            impl B {
-                                fn #fn_ident(&mut self, input: #param_ty) -> () {}
-                            }
-                        }.into());
+                        let varname = anomy_ident(&fn_ident);
+                        stream_pair.push((fn_ident, param_ty, varname));
                     }
                 },
                 _ => (),
@@ -37,7 +36,75 @@ pub fn derive(input: TokenStream) -> TokenStream {
         _ => (),
     }
 
-    let mut token_stream = proc_macro::TokenStream::new();
-    token_stream.extend(qoute_series.into_iter());
-    token_stream
+    let field_stream = create_stream(
+        stream_pair.iter().map(|(ident, ty, _varname)| {
+            quote::quote! { #ident: #ty, }
+        }).collect()
+    );
+
+    let op_field_stream = create_stream(
+        stream_pair.iter().map(|(_ident, ty, varname)| {
+            return quote::quote! { #varname: Option<#ty>, }
+        }).collect()
+    );
+
+    let default_stream = create_stream(
+        stream_pair.iter().map(|(_ident, _ty, varname)| {
+            quote::quote! { #varname: None, }
+        }).collect()
+    );
+
+    let method_stream = create_stream(
+        stream_pair.iter().map(|(ident, ty, varname)| {
+            quote::quote! {
+                impl OptionBuilderT {
+                    fn #ident(&mut self, input: #ty) -> () {
+                        self.#varname = Some(input);
+                    }
+                }
+            }
+        }).collect()
+    );
+
+    let build_stream = create_stream(
+        stream_pair.iter().map(|(ident, _ty, varname)| {
+            quote::quote! { #ident: self.#varname.unwrap().clone(), }
+        }).collect()
+    );
+
+    let struct_ident = input_stream.ident.to_token_stream();
+    quote::quote! {
+        impl #struct_ident {
+            fn builder() -> OptionBuilderT {
+                OptionBuilderT{..Default::default()}
+            }
+        }
+
+        pub struct BuilderT {
+            #field_stream
+        }
+
+        pub struct OptionBuilderT {
+            #op_field_stream
+        }
+
+        
+        impl Default for OptionBuilderT {
+            fn default() -> OptionBuilderT {
+                OptionBuilderT{
+                    #default_stream
+                }
+            }
+        }
+
+        #method_stream
+
+        impl OptionBuilderT {
+            fn build(self) -> Option<BuilderT> {
+                return Some(BuilderT{
+                    #build_stream
+                });
+            }
+        }
+    }.into()
 }
